1f9b072c70fb2d1ecc06b82250e0e52c7bb47b29
eli-bigman <nutsugah81@gmail.com>
2022-10-23 01:00:56 +0000

    files

---
+++ script.js
@@ -0,0 +1,28 @@
+class Calculator{
+    constructor(previousOperandTextElement,currentOperandTextElement){
+    this.previousOperandTextElement =previousOperandTextElement
+    this.currentOperandTextElement =currentOperandTextElement
+}
+}
+
+
+
+const numberButton = document.querrySelectAll(["data-number"])
+const operationButton = document.querrySelectAll(["data-operation"])
+const equaleButton = document.querySelector(["data-equals"])
+const deleteButton = document.querySelector(["data-delete"])
+const allClearButton = document.querySelector(["dat-all-clear"]) 
+const previousOperandTextElement = document.querrySelector(["data-previous-operand"])
+const currentOperandTextElement = document.querrySelector(["data-current-operand"])
+
+
+
+
+
+
+
+
+
+
+
+
